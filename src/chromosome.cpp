da8a6f04ce791d524445e4380362fd607f15671e
Hardikb09 <49986138+Hardikb09@users.noreply.github.com>
2019-04-25 19:13:37 +0530

    Create chromosome.cpp

---
+++ src/chromosome.cpp
@@ -0,0 +1,56 @@
+  #include<datastorage.hpp>
+  #include<vector>
+
+  using namespace std;
+
+  int Chromosome::getCid()
+  {
+    return cid;
+  }
+
+  int Chromosome::getTeamSize()
+  {
+     return teamSize;
+  }
+
+  int Chromosome::getFitnessVal()
+  {
+      return fitnessVal;
+  }
+  void Chromosome::setCid(int cid1)
+  {
+      cid=cid1;
+  }
+  void Chromosome::setTeamSize(int teamSize1)
+  {
+      teamSize = teamSize1;
+  }
+  void Chromosome::setGenes(vector<Gene> genes1)
+  {
+      copy(genes1.begin(), genes1.end(), back_inserter(genes)); 
+  }
+  void setFitnessVal(int fitnessVal1)
+  { 
+     fitnessVal = fitnessVal1;
+  }
+  void Chromosome::setGeneAtIndex(int i,Gene X)
+  {
+      genes[i]=X;
+  }
+  Gene Chromosome::getGeneWithID(int id)
+  {   
+      vector<Gene>::iterator i;
+      for(i=genes.begin();i!=genes.end(); ++i)
+      {
+        if( (*i).getGid() == id )
+         return (*i);
+      }
+      return NULL;
+  }
+  Gene Chromosome::getGeneAtIndex(int index)
+  {
+    return genes[index];
+  }    
+  void fitnessFunction();
+  };
+
